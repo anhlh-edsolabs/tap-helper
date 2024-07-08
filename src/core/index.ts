dc0c31942c1a4a809acba1b31048195ce718e949
Le Hoang Anh <anh.le@angelhack.com>
2024-07-08 14:34:33 +0700

    add: randombytes as rng to core package

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,8 +1,10 @@
 import * as bitcoin from "bitcoinjs-lib";
 import * as ecc from "tiny-secp256k1";
 import { ECPairFactory } from "ecpair";
+import { randomBytes as rng } from "crypto";
+
 bitcoin.initEccLib(ecc);
 
 const ECPair = ECPairFactory(ecc);
 
-export { bitcoin, ECPair, ecc };
+export { bitcoin, ECPair, ecc, rng };
